63819455008d2ceb0d3399b97b9f95c1466a2abe
Dameer Ahmed <dameerahmedghouri05@gmail.com>
2026-01-27 21:43:54 +0500

    basic chatbot  workflow

---
+++ Basics_workflows/01_basic_chatbot.py
@@ -0,0 +1,54 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
+from dotenv import load_dotenv
+
+load_dotenv()
+class ChatState(TypedDict):
+
+    messages: Annotated[list[BaseMessage], add_messages]
+
+llm=ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+)
+
+
+checkpointer = MemorySaver()
+def chat_node(state: ChatState):
+
+    
+    messages = state['messages']
+
+   
+    response = llm.invoke(messages)
+
+    
+    return {'messages': [response]}
+graph = StateGraph(ChatState)
+
+
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
+thread_id='1'
+
+while True:
+    message = input("You: ")
+    config={
+        'configurable':{'thread_id':thread_id},
+    }
+    if message.lower() == 'exit':
+        break
+    response = chatbot.invoke({'messages': [HumanMessage(content=message)]}, config=config)
+    print(f"Bot: {response['messages'][-1].content}")    
+    
+
+ 
